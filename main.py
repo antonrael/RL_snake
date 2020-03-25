c7e199055d4f45b4271a55decb0e4a83a4e2e58d
anton <anton;rael@polytechnique.edu>
2020-03-25 17:35:28 +0100

    add exploration graph change parameters clean code

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ tau = init_tau = 1
 tau_inc = 0.01
 gamma = 0.99
 epsilon = 0.5
-epsilon_decay = 0.99998
+epsilon_decay = 0.99997
 verbose = True
 
 # Define types of algorithms
@@ -37,7 +37,7 @@ Q_LEARNING = "Q_learning"
 EPSILON_GREEDY = "epsilon_greedy"
 SOFTMAX = "softmax"
 GREEDY = "greedy"
-VBDE_EPSILON = "vbde_epsilon"
+VDBE_EPSILON = "vdbe_epsilon"
 
 # Choose methods for learning and exploration
 rl_algorithm = args.rl_algorithm
@@ -46,9 +46,9 @@ n_episode = args.n_episode
 train = args.training
 print(rl_algorithm)
 
-#Parameters for VBDE
-delta = 0.25
-sigma = 5.0
+#Parameters for VDBE
+sigma = 10
+delta=1/3
 n_env = args.n_env
 
 
@@ -70,16 +70,18 @@ def act_with_epsilon_greedy(s, q, e):
     a = np.argmax(q[s])
     if np.random.rand() < e:
         a = np.random.randint(q.shape[-1])
-    return a
+        return a, 1
+    return a, 0
 
 def act_with_epsilon_softmax(s,q,e):
     a = np.argmax(q[s])
     if np.random.rand() < e:
         a = act_with_softmax(s,q)
-    return a
+        return a, 1
+    return a, 0
 
 #Update value of the epsilon table for VBED
-def update_epsilon_vbde(old_q,new_q,old_epsilon):
+def update_epsilon_vdbe(old_q,new_q,old_epsilon):
     if old_q==0 and new_q==0:
         return old_epsilon
     delta_q = abs(old_q-new_q)
@@ -119,40 +121,36 @@ def main():
     dim = list_s[:].tolist()
     dim.append(n_a)
 
-
-
     # Experimental setup
     print("n_episode ", n_episode)
     max_horizon = 500
     eval_steps = 10
 
-
-
     if train:
-        greedy_success_rate_monitor = np.zeros([n_episode,1])
-        greedy_discounted_return_monitor = np.zeros([n_episode,1])
-
-        behaviour_success_rate_monitor = np.zeros([n_episode,1])
-        behaviour_discounted_return_monitor = np.zeros([n_episode,1])
 
         # Init Q-table
 
         q_table = np.zeros(dim)
         env.reset()
 
-        # Init epsilon table for VBDE
+        # Init epsilon table for vdbe
         epsilon_table = np.ones(list_s)
 
         X=[]
         Y=[]
         Y_mean=[]
+        P=[]
 
         #init parameters graph
         mean_return = 0.0
         interval_graph = 50
 
+        #calculate rate of exploration
+        n_exploration=0
+        n_tot=0
         # Train for n_episode
         for i_episode in range(n_episode):
+            
             # Reset a cumulative reward for this episode
             total_return = 0.0
             
@@ -164,25 +162,36 @@ def main():
             if explore_method == SOFTMAX:
                 a = act_with_softmax(s, q_table)
             elif explore_method == EPSILON_GREEDY:
-                a = act_with_epsilon_greedy(s, q_table,epsilon)
-            elif explore_method == VBDE_EPSILON:
-                a = act_with_epsilon_softmax(s,q_table,epsilon_table[s])
+                a, exploration = act_with_epsilon_greedy(s, q_table,epsilon)
+                n_exploration+=exploration
+                n_tot+=1
+            elif explore_method == VDBE_EPSILON:
+                a, exploration = act_with_epsilon_softmax(s,q_table,epsilon_table[s])
+                n_exploration+=exploration
+                n_tot+=1
             else:
                 raise ValueError("Wrong Explore Method:".format(explore_method))
-
+            
 
             for i_step in range(max_horizon):
+                
                 # Act
                 s_prime, r, done, info = env.step(a)
-                total_return += r
+                #count the number of eaten apple
+                if r>1:
+                    total_return += 1
 
                 # Select an action
                 if explore_method == SOFTMAX:
                     a_prime = act_with_softmax(s_prime, q_table)
                 elif explore_method == EPSILON_GREEDY:
-                    a_prime = act_with_epsilon_greedy(s_prime, q_table,epsilon)
-                elif explore_method == VBDE_EPSILON:
-                    a_prime = act_with_epsilon_softmax(s_prime, q_table,epsilon_table[s_prime])
+                    a_prime, exploration = act_with_epsilon_greedy(s_prime, q_table,epsilon)
+                    n_exploration+=exploration
+                    n_tot+=1
+                elif explore_method == VDBE_EPSILON:
+                    a_prime, exploration = act_with_epsilon_softmax(s_prime, q_table,epsilon_table[s_prime])
+                    n_exploration+=exploration
+                    n_tot+=1
                 else:
                     raise ValueError("Wrong Explore Method:".format(explore_method))
 
@@ -200,20 +209,20 @@ def main():
                     raise ValueError("Wrong RL algorithm:".format(rl_algorithm))
 
                 #Update epsilon_table
-                if explore_method == VBDE_EPSILON:
+                if explore_method == VDBE_EPSILON:
                     new_q = q_table[base]
-                    epsilon_table[s] = update_epsilon_vbde(old_q,new_q,epsilon_table[s])
+                    epsilon_table[s] = update_epsilon_vdbe(old_q,new_q,epsilon_table[s])
 
                 # Transition to new state
                 s = s_prime
                 a = a_prime
                 if done:
-                    total_return+=0.1
                     break
-            if i_episode % 100 == 0:
+                    
+            if i_episode % 1000== 0:
                 if verbose:
-                    if explore_method == VBDE_EPSILON:
-                        print("Episode: {0}\t Num_Steps: {1:>4}\tMean_Return:{2:>5.2f}\tTotal_Return: {3:>5.2f}\tEpsilon: {4:.3f}".format(i_episode, i_step,mean_return, total_return, np.mean(epsilon_table)))
+                    if explore_method == VDBE_EPSILON:
+                        print("Episode: {0}\t Num_Steps: {1:>4}\tMean_Return:{2:>5.2f}\tTotal_Return: {3:>5.2f}\tMean_epsilon: {4:.3f}".format(i_episode, i_step,mean_return, total_return, np.mean(epsilon_table)))
                     else :    
                         print("Episode: {0}\t Num_Steps: {1:>4}\tMean_Return:{2:>5.2f}\tTotal_Return: {3:>5.2f}\tEpsilon: {4:.3f}".format(i_episode, i_step,mean_return, total_return, epsilon))
                     #print "Episode: {0}\t Num_Steps: {1:>4}\tTotal_Return: {2:>5.2f}\tTermR: {3}\ttau: {4:.3f}".format(i_episode, i_step, total_return, r, tau)
@@ -231,10 +240,14 @@ def main():
                 Y.append(total_return)
                 Y_mean.append(mean_return)
                 mean_return=0
+                if explore_method==EPSILON_GREEDY or explore_method == VDBE_EPSILON:
+                    P.append(n_exploration/n_tot)
+                    n_exploration=0
+                    n_tot=0
 
 
         q_flat = q_table.flatten()
-        np.save(f'q_saved_{rl_algorithm}_{explore_method}_{n_episode}_episodes_env_{n_env}.npy', q_flat)
+        np.save(f'RESULTS/q_saved_{rl_algorithm}_{explore_method}_{n_episode}_episodes_env_{n_env}.npy', q_flat)
 
         #Graph of results
         plt.xlabel('Episodes')
@@ -243,11 +256,19 @@ def main():
         plt.plot(X,Y,'ro',label='Total_return')
         plt.plot(X,Y_mean,'b+',label='Mean return')
         plt.legend()
-        plt.savefig(f'graph_{rl_algorithm}_{explore_method}_{n_episode}_episodes_env_{n_env}.png')
+        plt.savefig(f'RESULTS/graph_{rl_algorithm}_{explore_method}_{n_episode}_episodes_env_{n_env}.png')
         plt.show()
+        if explore_method==EPSILON_GREEDY or explore_method == VDBE_EPSILON:
+            plt.xlabel('Episodes')
+            plt.ylabel('Exploration rate')
+            plt.title(f'Exploration rate with {rl_algorithm} algorithm and {explore_method} in env {n_env}')
+            plt.plot(X,P)
+            plt.savefig(f'RESULTS/courbe_epsilon_{rl_algorithm}_{explore_method}_{n_episode}_episodes_env_{n_env}.png')
+            plt.show()
 
-    q_table = np.load(f'q_saved_{rl_algorithm}_{explore_method}_{n_episode}_episodes_env_{n_env}.npy').reshape(dim)
-    print(q_table)
+
+    q_table = np.load(f'RESULTS/q_saved_{rl_algorithm}_{explore_method}_{n_episode}_episodes_env_{n_env}.npy').reshape(dim)
+
     actions = []
     rew = []
     bonus = []
@@ -262,40 +283,8 @@ def main():
         if done:
             break
 
-    snake.main(actions, 10, 10, bonus)
+    snake_game.main(actions, 10, 10, bonus)
     exit(0)
-
-    plt.figure(0)
-    plt.plot(range(0,n_episode,10),greedy_success_rate_monitor[0::10,0])
-    plt.title("Greedy policy with {0} and {1}".format(rl_algorithm,explore_method))
-    plt.xlabel("Steps")
-    plt.ylabel("Success Rate")
-
-    plt.figure(1)
-    plt.plot(range(0,n_episode,10),behaviour_success_rate_monitor[0::10,0])
-    plt.title("Behaviour policy with {0} and {1}".format(rl_algorithm,explore_method))
-    plt.xlabel("Steps")
-    plt.ylabel("Success Rate")
-    plt.show()
-
-
-    #Show an episod
-
-    # for i_step in range(max_horizon):
-    #     env.render()
-    #     a = np.argmax(q_table[s])
-    #     s, r, done, info = env.step(a)
-    #     total_return += np.power(gamma,i_step) *r
-    #
-    #     if done:
-    #         print "Episode: {0}\t Num_Steps: {1:>4}\tTotal_Return: {2:>5.2f}\tFinal_Reward: {3}".format(1, i_step, total_return, r)
-    #         break
-    #
-    # # Show Policy
-    #
-    # for s in range(n_s):
-    #     actions = ['LEFT','DOWN','RIGHT','UP']
-    #     print(actions[np.argmax(q_table[s])])
 
 if __name__ == "__main__":
 
